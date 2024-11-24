ebee7dcb084f19354f232505f2efbcbbe17253c1
Lucas Tito <lucassga500@gmail.com>
2024-11-23 22:52:04 -0300

    Login UI made

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.titolucas.mindlink"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.titolucas.mindlink"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
