a86d933c5677958f2e90303613b71531a65a172f
Lucas Tito <lucassga500@gmail.com>
2025-01-26 10:26:54 -0300

    Ipv4 configuration no longer needed
    10.0.2.2 was given as BASE_URL, which return the hosts ipv4

--- app/src/main/java/com/titolucas/mindlink/apinetwork/RetrofitInstance.kt
+++ app/src/main/java/com/titolucas/mindlink/apinetwork/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://192.168.1.2:3000/mindlink/"
+    private const val BASE_URL = "http://10.0..2:3000/mindlink/"
 //    private const val BASE_URL = "http://172.25.215.8:3000/mindlink/"
 //    private const val BASE_URL = "http://192.168.0.4:3000/mindlink/"
 //private const val BASE_URL = "http://192.168.0.5:3000/mindlink/"
