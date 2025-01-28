2f7837a6ee7361dcf471410eb6e02ec8de73a6ea
Lucas Tito <lucassga500@gmail.com>
2025-01-27 23:52:42 -0300

    started to develop psycho home screen

--- app/src/main/java/com/titolucas/mindlink/PsychoHomeFragment.kt
+++ app/src/main/java/com/titolucas/mindlink/PsychoHomeFragment.kt
@@ -34,7 +34,7 @@ class PsychoHomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_home_psycho, container, false)
     }
 
     companion object {
